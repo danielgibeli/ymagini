41141c8d6394dc756019e83ab57fc45e8c9f781d
Daniel <danielgibeli@gmail.com>
2020-05-15 23:01:16 -0300

    Alterando DbContext

--- Ymagi/Data/YmagiContext.cs
+++ Ymagi/Data/YmagiContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ymagi.Models
 {
-    public class YmagiContext : DbContext
+    public class YmagiContext : IdentityDbContext
     {
         public YmagiContext (DbContextOptions<YmagiContext> options)
             : base(options)
